b502b0ff695637904d9243126a8a693076e31dfb
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-05-25 18:59:36 -0600

    add new file

---
+++ retringirRango.sh
@@ -0,0 +1,7 @@
+echo "Ingresa la ip inicial del rango"
+read q
+echo "Ingresa la ip final del rango"
+read w
+echo "Restringiendo rango..."
+echo "Rango de ip retringido: $q - $w"
+sudo /sbin/iptables -A INPUT -m iprange --src-range $q-$w -p tcp --dport 22 -j DROP
