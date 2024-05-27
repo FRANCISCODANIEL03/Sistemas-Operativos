b6d00a92545564e8dbd684412f84847721005701
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-05-26 21:55:28 -0600

    delete file

--- retringirRango.sh
+++
@@ -1,8 +0,0 @@
-#Codigo que pide un rango de ip's y lo restringe del puerto 22 tcp
-echo "Ingresa la ip inicial del rango"
-read q
-echo "Ingresa la ip final del rango"
-read w
-echo "Restringiendo rango..."
-echo "Rango de ip retringido: $q - $w"
-sudo /sbin/iptables -A INPUT -m iprange --src-range $q-$w -p tcp --dport 22 -j DROP
