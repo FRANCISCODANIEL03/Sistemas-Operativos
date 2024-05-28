ee6c118c6fdadae353882c547b0df0d74d6c46d3
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-05-27 21:55:33 -0600

    add new file

---
+++ IP'S/permitirMac.sh
@@ -0,0 +1,5 @@
+echo "Ingresa la direccion MAC a perimitir"
+read q
+echo "Permitiendo direccion MAC..."
+echo "Direccion Mac perimitida: $q"
+sudo /sbin/iptables -D INPUT -m mac --mac-source $q -j DROP
