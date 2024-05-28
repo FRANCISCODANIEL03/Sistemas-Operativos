cdc277011bda2d0841765d8c05677dac31b25165
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-05-27 21:55:40 -0600

    add new file

---
+++ IP'S/permitirRango.sh
@@ -0,0 +1,7 @@
+echo "Ingresa la ip inicial del rango"
+read q
+echo "Ingresa la ip final del rango"
+read w
+echo "Perimitiendo rango..."
+echo "Rango de ip perimitido: $q - $w"
+sudo /sbin/iptables -D INPUT -m iprange --src-range $q-$w -p tcp --dport 22 -j DROP
